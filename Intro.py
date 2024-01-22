a67f5930f0b4b1824f7f06517005194ea4b77b32
lunaknight09 <130379665+lunaknight09@users.noreply.github.com>
2024-01-22 14:43:01 -0500

    Add files via upload

---
+++ Intro.py
@@ -0,0 +1,22 @@
+name = 'Nathan'
+age = '32'
+town = 'St. Louis'
+townA = 'Bloomington'
+hobby = 'Video games, manga, comics, and trading card games'
+major = 'Software Development'
+year = '2020'
+daughter = '2 daughters'
+dream = 'go to a professional lacrosse game'
+sports = 'rugby, kabaddi, cricket'
+ball = 'baseball, football, and soccer'
+
+print(f"Hello, my name is {name} and I am a {major} major.")
+print(f"I am {age} and originally from {town}.")
+print(f"I live in {townA} since {year}.")
+print(f"{hobby} are some hobbies I enjoy in my freetime.")
+print(f"With my {hobby} I turned some into a source of income by selling and trading.")
+print(f"{name} has {daughter} that he loves dearly.")
+print(f"{daughter} are 2 years old and 1 month old.")
+print(f"I would love to move back to {town} but the area since {year} has gotten worse to raise {daughter}.")
+print(f"I would love to {dream} as I love sports and that is my favorite currently.")
+print(f"Other sports I enjoy are {sports} besides the popular {ball}.")      
