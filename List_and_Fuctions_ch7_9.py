0d489a8f24211dc2a2419ddd980ac144a94feae5
lunaknight09 <130379665+lunaknight09@users.noreply.github.com>
2024-02-05 18:04:58 -0500

    reupload with requirements.txt file

--- List_and_Fuctions_ch7_9.py
+++ List_and_Fuctions_ch7_9.py
@@ -1,6 +1,8 @@
 #Nathan Hopkins
 #Module 3 Lists and Functions Programming assignment
 
+import requirements.txt
+
 # 7.4 Make a list called things with these three strings as elements: "mozzarella", "cinderella", "salmonella".
 things = ["mozzarella", "cinderella", "salmonella"]
 
