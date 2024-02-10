e7b6fa1bd1d5a8ba1ca2c5dce9a20a82476faed8
lunaknight09 <130379665+lunaknight09@users.noreply.github.com>
2024-02-10 13:36:24 -0500

    does not have 16.8 included

---
+++ zoo_hours_nonimport.py
@@ -0,0 +1,11 @@
+#Nathan Hopkins
+#Feb 10, 2024
+#11.1 second part after making zoo.py
+import zoo
+zoo.hours()
+#bug report I added the string of open 9-5 daily but did not need to run like I thought
+
+#11.2 
+import zoo as menagerie
+menagerie.hours()
+#bug report everything worked fine on this part as it is the same as above
